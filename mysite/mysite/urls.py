e194dc48056f9ee270515dab413a30be547ac5c1
Dennis Wong <dewong4@Denniss-MacBook-Air.local>
2017-07-20 16:01:48 -0700

    Files for django tutorial part 3.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,5 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^webapp/', include('webapp.urls')),
+    url(r'^$', include('personal.urls')),
 ]
